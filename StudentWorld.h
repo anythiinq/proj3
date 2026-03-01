58d541c5aafc02b38aad13408914369f713e5e37
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-03-01 14:36:57 -0800

    Add tool management and accessors in StudentWorld

--- StudentWorld.h
+++ StudentWorld.h
@@ -6,6 +6,9 @@
 #include "Actor.h"
 #include <string>
 #include <vector>
+#include <map>
+
+using namespace std;
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
@@ -18,9 +21,32 @@ public:
     virtual void cleanUp();
     
     bool hasSolidBrick(Coord c);
+    
+    // accessors
+    int returnTimeLeft() { return m_timeLeft; }
+    
+    int determineAttractionDirection(Coord c_lemming);
+    bool isClimbableAt(Coord c);
+    Lemming* getBounceableLemmingAt(Coord c);
+    bool isEmpty(Coord c);                             // returns if a square at c is empty or not (no actors at all)
+    
+    map<char, int> m_toolCounts;
+    
+    bool toolAvailable(char c);
+    void consumeTool(char c);
+    void placeTool(char tool, Coord c);
+    
 private:
-    std::string getCurrentLevel();
-    std::vector<Actor*> m_actors;
+    string getCurrentLevel();
+    vector<Actor*> m_actors;
+    Player* m_player;
+    
+    int m_timeLeft;
+    int m_nDeadLemmings;
+    int m_nSavedLemmings;
+    int m_nSpawnedLemmings;
+    int countLemmingsAlive();
+    bool isLemming();
 };
 
 #endif // STUDENTWORLD_H_
